297518d2940e37504d44578435ab1c3e943b476f
soulayy <s.sultaway@gmail.com>
2024-02-26 21:07:52 +0100

    maj

--- src/Compoments/Secondcomp/Secondcomp.jsx
+++ src/Compoments/Secondcomp/Secondcomp.jsx
@@ -1,4 +1,4 @@
-import {useState } from "react";
+import {useState, useRef} from "react";
 import "./Secondcomp.css";
 
 
@@ -6,51 +6,59 @@ export default function Secondcomp(props) {
 
 
 
+    
+    const [firstM, setFirstM] = useState(true)
+
+    const checkHandler = () => {
+        setFirstM(!firstM)
+      }
+
     return (
         <div className="h-[95%] w-[95%] flex justify-center items-center flex-col">
-            <div className="h-[90%] w-[90%] flex justify-center items-center flex-col">
+            <div className="h-[90%] w-[85%] flex justify-center items-center flex-col">
                 <div className="h-[15%] w-[80%] flex flex-col justify-center items-start gap-2">
                     <h1 className="text-5xl text-blue-700">Select your plan</h1>
                     <p className="text-gray-400">You have the option of montly or yearly biling</p>
                 </div>
-                <div className="h-[50%] w-[80%] flex justify-center items-center gap-2">
+                <div className="divhover h-[50%] w-[80%] flex justify-center items-center gap-2">
                     <div className="h-[60%] w-[40%] border-2 flex justify-around items-start flex-col p-3 rounded-xl">
                         <div className="h-[60%]">
 
-                        <img src={props.Arcade} alt="" />
+                            <img src={props.Arcade} alt="" />
                         </div>
                         <h1>Arcade</h1>
                         <p>{
+                            firstM == true ? "$9/mo" : "$90/yr"
                         }</p>
                     </div>
                     <div className="h-[60%] w-[40%] border-2 flex justify-around items-start flex-col p-3 rounded-xl">
-                    <div className="h-[60%]">
+                        <div className="h-[60%]">
 
-                        <img src={props.Advanced} alt="" />
+                            <img src={props.Advanced} alt="" />
                         </div>
 
                         <h1>Advanced</h1>
-                        {/* {
-                            activemoi == true ? <p>$9/mo</p> : <p>$60/mo</p>
-                        } */}
+                        <p>{
+                            firstM == true ? "$12/mo" : "$120/yr"
+                        }</p>
                     </div>
                     <div className="h-[60%] w-[40%] border-2 flex justify-around items-start flex-col p-3 rounded-xl">
-                    <div className="h-[60%]">
-
-                        <img src={props.Pro} alt="" />
+                        <div className="h-[60%]">
                         
+                            <img src={props.Pro} alt="" />
+
                         </div>
 
                         <h1>Arcade</h1>
-                        {/* {
-                            toBeChecked == true ? <p>$9/mo</p> : <p>$60/mo</p>
-                        } */}
+                        <p>{
+                            firstM == true ? "$15/mo" : "$150/yr"
+                        }</p>
                     </div>
                 </div>
                 <div className="form-control w-[80%] bg-slate-500">
                     <label className="label cursor-pointer w-[90%] text-blue-950 font-bold">
                         <p>Monthly</p>
-                        <input type="checkbox" className="toggle"/> 
+                        <input onChange={checkHandler} type="checkbox" className="toggle" value="false"/> 
                         <p>Yearly</p>
 
                     </label>
--- src/Compoments/Treecomp/Treecomp.jsx
+++ src/Compoments/Treecomp/Treecomp.jsx
@@ -1,30 +1,52 @@
 import React from 'react'
 
-export default function Treecomp() {
+export default function Treecomp(props) {
   return (
     <div className="h-[95%] w-[95%] flex justify-center items-center flex-col">
             <div className="h-[90%] w-[90%] flex justify-center items-center flex-col">
                 <div className="h-[15%] w-[80%] flex flex-col justify-center items-start gap-2">
-                    <h1 className="text-5xl text-blue-700">wayawwwwwwwwwwwwwwwwwwwwwwwwww</h1>
-                    <p className="text-gray-400">Please make provide your name, email adress, and phone number.</p>
+                    <h1 className="text-5xl text-blue-700">Pick add-ons</h1>
+                    <p className="text-gray-400">add on help enhance your game gaming experience</p>
                 </div>
                 <div className="h-[60%] w-[80%] flex justify-center items-start flex-col gap-5">
-                    <p className="text-blue-800 font-bold">Your name is : </p> 
-                    <label className="w-[80%] input input-bordered flex items-center gap-2">
-                        <input type="text" className="grow" placeholder="Vingt.six" />
-                    </label>
-                    <p className="text-blue-800 font-bold">Your email is : </p> 
-                    <label className="w-[80%] input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="vinght_six@email.com" />
-                    </label>
-                    <p className="text-blue-800 font-bold">Your phone number is : </p> 
-                    <label className="w-[80%] input input-bordered flex items-center gap-2">
-                        <input type="text" className="grow" placeholder="e.g+1 234 567 890" />
-                    </label>
+                    <div className='flex justify-between items-center w-[80%] bg-blue-400'>
+                      <input type="checkbox" defaultChecked className="checkbox" />
+                      <div className='flex justify-start items-start flex-col w-[70%]'>
+                        <h1>Online service</h1>
+                        <p>Acces to multiplayers games</p>
+                      </div>
+                      <div>
+                        <p>+10/yr</p>
+                      </div>
+                    </div>
+                    <div className='flex justify-between items-center w-[80%] bg-blue-400'>
+                    <input type="checkbox" defaultChecked className="checkbox" />
+                      <div className='flex justify-start items-start flex-col w-[70%]'>
+                        <h1>Online service</h1>
+                        <p>Acces to multiplayers games</p>
+                      </div>
+                      <div>
+                        <p>+10/yr</p>
+                      </div>
+                    </div>
+                    <div className='flex justify-between items-center w-[80%] bg-blue-400'>
+                    <input type="checkbox" defaultChecked className="checkbox" />
+                      <div className='flex justify-start items-start flex-col w-[70%]'>
+                        <h1>Online service</h1>
+                        <p>Acces to multiplayers games</p>
+                      </div>
+                      <div>
+                        <p>+10/yr</p>
+                      </div>
+                    </div>
                 </div>
-                <div className="h-[15%] w-[50%] flex justify-end items-end">
+
+                <div className="h-[15%] w-[80%] flex justify-between items-end">
+                    <button className="text-white h-11 w-28 rounded-xl bg-blue-950" onClick={() => (props.setsecondvalue(true), props.settreevalue(false))}>BACK STEP</button>
+                    <button className="text-white h-11 w-28 rounded-xl bg-blue-950" onClick={() => (props.settreevalue(false), props.settreevalue(true))}>NEXT STEP</button>
+                </div>
               </div>
             </div>
-        </div>
+        
   )
 }
